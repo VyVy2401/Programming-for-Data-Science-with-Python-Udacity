fdae76effd88831cd60be6c37b1828ca47989276
VyVy2401 <119021490+VyVy2401@users.noreply.github.com>
2022-11-28 14:15:10 +0700

    Update bikeshare.py
    name

--- bikeshare.py
+++ bikeshare.py
@@ -191,7 +191,7 @@ def trip_duration_stats(df):
 def user_stats(df):
     """Displays statistics on bikeshare users."""
 
-    print('\nCalculating User Stats...\n')
+    print('\nCalculating User Stats..\n')
     start_time = time.time()
 
 
